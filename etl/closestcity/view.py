61cdff54bcac54243353fbf7f41c37400d418bc7
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2020-12-11 18:16:24 +0000

    feat: implement generate geometry view (#2)
    * updated postgres image to be postgis image
    
    * implemented generate view

---
+++ etl/closestcity/view.py
@@ -0,0 +1,32 @@
+import sqlalchemy
+from sqlalchemy.engine import Engine
+import logging
+import psycopg2
+
+logger = logging.getLogger(__name__)
+
+
+def create_view(**kwargs):
+
+    worldcities_file = kwargs.get('worldcities_file')
+    srid = kwargs.get('srid')
+    publish = kwargs.get('publish')
+
+    table = worldcities_file.split('.')[0]
+
+    engine: Engine = sqlalchemy.create_engine(kwargs.get('write_url'))
+    create_view = f'CREATE MATERIALIZED VIEW {table}_geometry AS select *, ST_SetSRID(ST_MakePoint(lng, lat), {srid}) as geom from {table}'
+
+    logger.info(f'Applying {create_view}')
+
+    if publish:
+
+        try:
+            engine.execute(create_view)
+        except sqlalchemy.exc.ProgrammingError as e:
+            if isinstance(e.orig, psycopg2.errors.DuplicateTable):
+                logger.warning('View already existed, skipping')
+            else:
+                raise
+    else:
+        logger.warning('Publish flag was not set, skipping')

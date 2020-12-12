2ffa57b9967e42a25eb66f7462e38173c8a67e2c
kiran94 <kiran_patel94@hotmail.com>
2020-12-12 12:52:24 +0000

    corrected port in main

--- etl/closestcity/__main__.py
+++ etl/closestcity/__main__.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     parser.add_argument('--worldcities_file', default='worldcities.csv')
     parser.add_argument(
         '--write_url', 
-        default=f'postgres://{os.environ["PGUSER"]}:{os.environ["PGPASSWORD"]}@{os.environ.get("PGHOST", "localhost")}:{os.environ.get("PGHOST", "5432")}/{os.environ.get("PGDATABASE", "closestcity")}')
+        default=f'postgres://{os.environ["PGUSER"]}:{os.environ["PGPASSWORD"]}@{os.environ.get("PGHOST", "localhost")}:{os.environ.get("PGPORT", "5432")}/{os.environ.get("PGDATABASE", "closestcity")}')
     parser.add_argument('--publish', action='store_true')
 
     args = parser.parse_args()
